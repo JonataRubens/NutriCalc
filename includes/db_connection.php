a4e3272c3975ba63011d819548192bd6fa9308a6
Galessss <marcus.balbino.2001@gmail.com>
2025-04-22 20:14:23 -0300

    alteracao n 8

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,18 +1,13 @@
 <?php
-/*
+
 // Database configuration
 $host = "db"; // Database host
 $port = "3306"; // Database port 
 $username = "admin";
 $password = "admin"; 
 $database = "nutricalc"; 
-*/
-// Database configuration
-$host = "localhost"; // Database host
-$port = "3306"; // Database port 
-$username = "root";
-$password = ""; 
-$database = "nutricalc"; 
+
+
 
 
 // Create connection
